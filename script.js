91555346a916f42bc3d70817e697f52d33f46127
sakazukiAoi <52361879+sakazukiAoi@users.noreply.github.com>
2025-03-23 20:28:49 +0900

    Update script.js

--- script.js
+++ script.js
@@ -8,12 +8,15 @@ function setup() {
     let canvas = createCanvas(windowWidth, windowHeight);
     canvas.parent('canvas-container');
     document.getElementById('canvas-container').style.display = 'none'; // 初期状態で非表示
+    imageMode(CORNER); // 画像描画モードを左上基準に
 }
 function handleGyro(event) {
     let hueVal = map(event.gamma, -45, 45, 180, 300, true); // 色相変化を抑える
-    tint(color(`hsl(${hueVal}, 100%, 50%)`)); // 色相変更
+    push();
+    colorMode(HSB, 360, 100, 100);
+    tint(hueVal, 100, 100); // HSBで色相変更
     image(imgB, 0, 0, width, height); // 背景にBの画像を描画
-    noTint(); // 元の色に戻す
+    pop();
     image(imgA, 0, 0, width, height); // 穴あき絵を前面に描画
 }
 document.getElementById('enableGyro').addEventListener('click', function() {
